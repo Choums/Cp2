b67eef0441e026fa02698eedc238ef79ecaaa9e7
Chams AIDEL <chaidel@e2r5p9.clusters.42paris.fr>
2022-11-08 15:41:05 +0100

    ex01 - done, les bits sont bouge en integral ou mantissa pour ne pas perdre l'info pour afficher en float

--- ex01/src/Fixed.cpp
+++ ex01/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: chaidel <chaidel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 11:19:40 by chaidel           #+#    #+#             */
-/*   Updated: 2022/11/08 14:18:49 by chaidel          ###   ########.fr       */
+/*   Updated: 2022/11/08 15:39:11 by chaidel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,15 @@ Fixed::Fixed(Fixed const& cpy)
 /*	Convertit l'integer en fixed number */
 Fixed::Fixed(const int num)
 {
-	this->_value = (num >> Fixed::_bits / 2);
+	std::cout << "Int constructor called" << std::endl;
+	this->_value = (num << Fixed::_bits);
 }
 
 /*	Convertit le float en fixed number */
 Fixed::Fixed(const float num)
 {
-	this->_value = roundf(num) * (1 << Fixed::_bits);
+	std::cout << "Float constructor called" << std::endl;
+	this->_value = roundf(num * (1 << Fixed::_bits));
 }
 
 Fixed::~Fixed()
@@ -45,15 +47,12 @@ Fixed::~Fixed()
 
 int	Fixed::toInt(void) const
 {
-	int	num;
-	// num = roundf(this->_value) << Fixed::_bits;
-	return (num);
+	return (this->_value >> Fixed::_bits);
 }
 
 float	Fixed::toFloat(void) const
 {
-	float	num;
-	return (num);
+	return ((float)this->_value / (float)(1 << Fixed::_bits));
 }
 
 int	Fixed::getRawBits(void) const
@@ -67,8 +66,6 @@ void	Fixed::setRawBits(int const raw)
 	this->_value = raw;
 }
 
-
-
 Fixed&	Fixed::operator=(Fixed const& obj)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
@@ -79,6 +76,6 @@ Fixed&	Fixed::operator=(Fixed const& obj)
 
 std::ostream&	operator<<(std::ostream& flux, Fixed const& num)
 {
-	flux << num.getRawBits();
+	flux << num.toFloat();
 	return (flux);
 }
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chaidel <chaidel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 11:19:52 by chaidel           #+#    #+#             */
-/*   Updated: 2022/11/08 11:19:54 by chaidel          ###   ########.fr       */
+/*   Updated: 2022/11/08 15:34:25 by chaidel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 
 int	main(void)
 {
-	Fixed	a;
-	Fixed	b( a );
-	Fixed	c;
-
-	c = b;
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
 	
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	a = Fixed( 1234.4321f );
 	
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return (0);
 }
